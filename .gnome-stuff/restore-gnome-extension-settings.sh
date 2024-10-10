8010b21ef20232100fed8314449a6aae8d10f20f
botn0ught <mayank.sinha02@gmail.com>
2024-10-10 22:31:45 +0530

    check for installed gnome exts before restoring settings

--- .gnome-stuff/restore-gnome-extension-settings.sh
+++ .gnome-stuff/restore-gnome-extension-settings.sh
@@ -1,12 +1,48 @@
-#!/bin/bash
+requiredExts=("caffeine@patapon.info" "Vitals@CoreCoding.com" "blur-my-shell@aunetx" "dash-to-panel@jderose9.github.com")
 
-echo "Restoring dash-to-panel settings..."
-dconf load /org/gnome/shell/extensions/dash-to-panel/ < gnome-extension-settings/dtp_settings
-echo "Restoring caffeine settings..."
-dconf load /org/gnome/shell/extensions/caffeine/ < gnome-extension-settings/caffeine_settings
-echo "Restoring blur-my-shell settings..."
-dconf load /org/gnome/shell/extensions/blur-my-shell/ < gnome-extension-settings/bms_settings
-echo "Restoring Vitals settings..."
-dconf load /org/gnome/shell/extensions/Vitals/ < gnome-extension-settings/vitals_settings
+output=$(gsettings get org.gnome.shell enabled-extensions)
 
-echo "All Done!"
+set -f
+exts="${output//[\[\]\',]/}"
+
+function exists_in_list() {
+    LIST=$1
+    VALUE=$2
+    for x in ${LIST[@]}; do
+	if [ "$x" = "$VALUE" ]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+
+function restore_settings() {
+    if [ "$1" = "dash-to-panel@jderose9.github.com" ]; then
+	dconf load /org/gnome/shell/extensions/dash-to-panel/ < gnome-extension-settings/dtp_settings
+    elif [ "$1" = "caffeine@patapon.info" ]; then
+	dconf load /org/gnome/shell/extensions/caffeine/ < gnome-extension-settings/caffeine_settings
+    elif [ "$1" = "blur-my-shell@aunetx" ]; then
+	dconf load /org/gnome/shell/extensions/blur-my-shell/ < gnome-extension-settings/bms_settings
+    elif [ "$1" = "Vitals@CoreCoding.com" ]; then
+	dconf load /org/gnome/shell/extensions/Vitals/ < gnome-extension-settings/vitals_settings
+    else
+	echo "No backup found for $1"
+    fi
+    echo "Done"
+}
+
+for i in ${!requiredExts[@]}; do
+  currentExt=${requiredExts[i]}
+  if exists_in_list "$exts" "$currentExt"; then
+    echo ""
+    echo "Restoring Settings for $currentExt..."
+    restore_settings $currentExt
+  else
+    echo ""
+    echo "$currentExt is not installed!"
+    echo "Please install $currentExt and run this script again!"
+  fi
+done
+
+echo ""
